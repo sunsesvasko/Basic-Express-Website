fc375487d0a4bb36d7ba33c1d53c745851c77900
Vasko <vbblajev@gmail.com>
2023-10-25 17:43:50 +0300

    Creating views, routers and setting them up

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// === 0) Modules === //
 const path = require('path');
 const http = require('http');
 const dotenv = require('dotenv');
@@ -9,18 +10,30 @@ const port = process.env.PORT;
 
 const app = express();
 
-// === Middlewares === //
+// Set view engine and views folder
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
+// === 1) Middlewares === //
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: false, limit: '10kb' }));
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-// === Routes === //
+// === 2) Routes === //
 app.get('/', (req, res) => {
 
-    res.status(200).send('Hello World');
+    res.status(200).render('index', {
+        title: 'Welcome'
+    });
 })
 
-// === Server === //
+app.get('/about', (req, res) => {
+
+    res.status(200).render('about')
+})
+
+// === 3) Server === //
 app.listen(port);
 console.log(`Server is running on port ${port}`);

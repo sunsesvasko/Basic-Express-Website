8abf8b8c0f9aeaaee8d3a3a5eeebfb5aadc8b270
Vasko <vbblajev@gmail.com>
2023-10-26 14:15:58 +0300

    Database connection established + server & app are separated in different files

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 // === 0) Modules === //
 const path = require('path');
 const http = require('http');
-const dotenv = require('dotenv');
 const express = require('express');
 
 const viewRouter = require('./routes/viewRoutes');
-
-dotenv.config({ path: './config.env' });
-const port = process.env.PORT;
 
 const app = express();
 
@@ -25,6 +21,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 // === 2) Routes === //
 app.use('/', viewRouter);
 
-// === 3) Server === //
-app.listen(port);
-console.log(`Server is running on port ${port}`);
+module.exports = app;

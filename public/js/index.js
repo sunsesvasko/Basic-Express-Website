b61a3e7ee4f3944a36f3104f1915f62ee5fbcf84
Vasko <vbblajev@gmail.com>
2023-10-27 21:41:21 +0300

    Implementing Signup Functionality

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,10 @@
 import { showAlert } from './alerts';
 import { login, logout } from './login';
+import { signup } from './signup';
 
 const loginForm = document.querySelector('.form--login');
 const logOutBttn = document.querySelector('#logout--btn');
+const signUpForm = document.querySelector('.form--signup');
 
 if(loginForm) {
     loginForm.addEventListener('submit', (e) => {
@@ -18,4 +20,15 @@ if(logOutBttn) {
         e.preventDefault();
         logout();
     });
+}
+
+if(signUpForm) {
+    signUpForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const username = document.querySelector('#username').value;
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#pass').value;
+        const passwordConfirm = document.querySelector('#passConfirm').value;
+        signup(username, email, password, passwordConfirm);
+    })
 }
